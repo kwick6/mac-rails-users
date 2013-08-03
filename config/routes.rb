1b234c2d75a0146f6ab16b6ed0b475ea29f7d4dd
SG <sy1840@gmail.com>
2013-08-03 11:01:49 -0700

    deploy heroku

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ get '/all', :to => 'comments#index', :as => 'all'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+   root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
